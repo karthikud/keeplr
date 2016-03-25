2be299d11e154e74e516ed08994d4970bf8f4f44
karthikud <karthikud@gmail.com>
2016-03-25 16:06:18 +0530

    Update todos.js

--- public/js/services/todos.js
+++ public/js/services/todos.js
@@ -1,17 +1,17 @@
-angular.module('todoService', [])
+angular.module('bookmarkService', [])
 
 	// super simple service
 	// each function returns a promise object 
-	.factory('Todos', ['$http',function($http) {
+	.factory('BookMarks', ['$http',function($http) {
 		return {
 			get : function() {
-				return $http.get('/api/todos');
+				return $http.get('/api/bookmarks');
 			},
-			create : function(todoData) {
-				return $http.post('/api/todos', todoData);
+			create : function(bookmarkData) {
+				return $http.post('/api/bookmarks', bookmarkData);
 			},
 			delete : function(id) {
-				return $http.delete('/api/todos/' + id);
+				return $http.delete('/api/bookmarks/' + id);
 			}
 		}
 	}]);
