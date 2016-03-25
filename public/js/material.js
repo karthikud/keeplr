fddb6ffed63856b36d241409c247441d941e1078
karthikud <karthikud@gmail.com>
2016-03-25 18:14:55 +0530

    Update material.js

--- public/js/material.js
+++ public/js/material.js
@@ -65,6 +65,22 @@ angular.module('StarterApp', ['bookmarkService','ngMaterial', 'ngMdIcons'])
       $scope.alert = 'You cancelled the dialog.';
     });
   };
+  
+  //add dialog
+   $scope.showAddcategory = function(ev) {
+    $mdDialog.show({
+       
+      controller: DialogController,
+      //templateUrl: 'Dialog.tmpl.html',
+      //template: '<md-dialog aria-label="Mango (Fruit)"> <md-content class="md-padding"> <form name="userForm"> <div layout layout-sm="column"> <md-input-container flex> <label>First Name</label> <input ng-model="user.firstName" placeholder="Placeholder text"> </md-input-container> <md-input-container flex> <label>Last Name</label> <input ng-model="theMax"> </md-input-container> </div> <md-input-container flex> <label>Address</label> <input ng-model="user.address"> </md-input-container> <div layout layout-sm="column"> <md-input-container flex> <label>City</label> <input ng-model="user.city"> </md-input-container> <md-input-container flex> <label>State</label> <input ng-model="user.state"> </md-input-container> <md-input-container flex> <label>Postal Code</label> <input ng-model="user.postalCode"> </md-input-container> </div> <md-input-container flex> <label>Biography</label> <textarea ng-model="user.biography" columns="1" md-maxlength="150"></textarea> </md-input-container> </form> </md-content> <div class="md-actions" layout="row"> <span flex></span> <md-button ng-click="answer(\'not useful\')"> Cancel </md-button> <md-button ng-click="answer(\'useful\')" class="md-primary"> Save </md-button> </div></md-dialog>',
+      targetEvent: ev,
+    })
+    .then(function(data) {
+      $scope.alert = 'You said the information was "' + data + '".';
+    }, function() {
+      $scope.alert = 'You cancelled the dialog.';
+    });
+  };
 }])
 
 .controller('ListBottomSheetCtrl', function($scope, $mdBottomSheet) {
@@ -105,9 +121,6 @@ angular.module('StarterApp', ['bookmarkService','ngMaterial', 'ngMdIcons'])
     .iconSet('communication', 'img/icons/sets/communication-icons.svg', 24)
     .defaultIconSet('img/icons/sets/core-icons.svg', 24);
 });
-
-;
-
 function DialogController($scope, $mdDialog,bookmark,BookMarks) {
   $scope.bookmark = {};
 		$scope.loading = true;
