85364c63dd88c5941009b2953bff60e5375d0fc2
karthikud <karthikud@gmail.com>
2016-03-25 15:25:23 +0530

    Update material.js

--- public/js/material.js
+++ public/js/material.js
@@ -4,7 +4,7 @@ app.controller('AppCtrl', ['$scope', '$mdBottomSheet','$mdSidenav', '$mdDialog',
   $scope.toggleSidenav = function(menuId) {
     $mdSidenav(menuId).toggle();
   };
-  
+  $scope.items =[{url:'https://www.google.com/',category:'Shopping']
  	$scope.menu = [
     {
       link : '',
@@ -34,38 +34,7 @@ app.controller('AppCtrl', ['$scope', '$mdBottomSheet','$mdSidenav', '$mdDialog',
       icon: 'settings'
     }
   ];
-  $scope.activity = [
-      {
-        what: 'Brunch this weekend?',
-        who: 'Ali Conners',
-        when: '3:08PM',
-        notes: " I'll be in your neighborhood doing errands"
-      },
-      {
-        what: 'Summer BBQ',
-        who: 'to Alex, Scott, Jennifer',
-        when: '3:08PM',
-        notes: "Wish I could come out but I'm out of town this weekend"
-      },
-      {
-        what: 'Oui Oui',
-        who: 'Sandra Adams',
-        when: '3:08PM',
-        notes: "Do you have Paris recommendations? Have you ever been?"
-      },
-      {
-        what: 'Birthday Gift',
-        who: 'Trevor Hansen',
-        when: '3:08PM',
-        notes: "Have any ideas of what we should get Heidi for her birthday?"
-      },
-      {
-        what: 'Recipe to try',
-        who: 'Brian Holt',
-        when: '3:08PM',
-        notes: "We should eat this: Grapefruit, Squash, Corn, and Tomatillo tacos"
-      },
-    ];
+ 
     $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
   $scope.alert = '';
   $scope.showListBottomSheet = function($event) {
@@ -81,6 +50,9 @@ app.controller('AppCtrl', ['$scope', '$mdBottomSheet','$mdSidenav', '$mdDialog',
   
   $scope.showAdd = function(ev) {
     $mdDialog.show({
+       locals: {
+           items: $scope.items
+         },
       controller: DialogController,
       templateUrl: 'tabDialog.tmpl.html',
       //template: '<md-dialog aria-label="Mango (Fruit)"> <md-content class="md-padding"> <form name="userForm"> <div layout layout-sm="column"> <md-input-container flex> <label>First Name</label> <input ng-model="user.firstName" placeholder="Placeholder text"> </md-input-container> <md-input-container flex> <label>Last Name</label> <input ng-model="theMax"> </md-input-container> </div> <md-input-container flex> <label>Address</label> <input ng-model="user.address"> </md-input-container> <div layout layout-sm="column"> <md-input-container flex> <label>City</label> <input ng-model="user.city"> </md-input-container> <md-input-container flex> <label>State</label> <input ng-model="user.state"> </md-input-container> <md-input-container flex> <label>Postal Code</label> <input ng-model="user.postalCode"> </md-input-container> </div> <md-input-container flex> <label>Biography</label> <textarea ng-model="user.biography" columns="1" md-maxlength="150"></textarea> </md-input-container> </form> </md-content> <div class="md-actions" layout="row"> <span flex></span> <md-button ng-click="answer(\'not useful\')"> Cancel </md-button> <md-button ng-click="answer(\'useful\')" class="md-primary"> Save </md-button> </div></md-dialog>',
