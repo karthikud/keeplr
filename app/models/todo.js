f19fab4e08c81846c1c62a1ab8d695ff581b4850
karthikud <karthikud@gmail.com>
2016-03-25 13:25:57 +0530

    Update todo.js
    added new models

--- app/models/todo.js
+++ app/models/todo.js
@@ -1,8 +1,21 @@
 var mongoose = require('mongoose');
 
-module.exports = mongoose.model('Todo', {
-    text: {
-        type: String,
-        default: ''
-    }
+var categorySchema = Schema({
+  _id     : Number,
+  name    : String,
+  personal     : Number,
+  bookmarks : [{ type: Schema.Types.ObjectId, ref: 'Bookmark' }],
+  collaborators     : [{ type: Number, ref: 'User' }]
 });
+
+var bookmarkSchema = Schema({
+  _belongs : { type: Number, ref: 'Category' },
+  title    : String,
+  _creator : { type: Number, ref: 'User' },
+});
+module.exports = {
+    mongoose.model(mongoose.model('Category', categorySchema));
+    mongoose.model(mongoose.model('BookMark', bookmarkSchema));
+}
+
+
