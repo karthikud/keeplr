5439335e0faa617de485008efd62448cb140b0ad
KRDS-RAVEE <karthikud@gmail.com>
2016-03-29 11:39:01 +0530

    changed google app id

--- config/auth.js
+++ config/auth.js
@@ -3,9 +3,9 @@
 // expose our config directly to our application using module.exports
 module.exports = {
    'googleAuth' : {
-        'clientID'      : '310954440372-82a3tevmrnnd16skr4m0c240m6gehcc6.apps.googleusercontent.com',
-        'clientSecret'  : 'm2ACtfxCB0F8XrUzce1qQRAJ',
-        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+        'clientID'      : '675538132205-rumluq6t6aab9729gmtdm67roh4mi833.apps.googleusercontent.com',
+        'clientSecret'  : 'C47GwbWUWla79EuYxSfe8O_7',
+        'callbackURL'   : 'http://keeplr.herokuapp.com/auth/google/callback'
     }
 
 };
